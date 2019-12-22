fe55001f26c1e030a6963e5035f3d2b551d71ff3
Ryuji Tsutsui <ryu22e@gmail.com>
2019-12-22 14:57:35 +0900

    Add password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -24,4 +24,20 @@ urlpatterns = [
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    path(
+        'password-reset-done/',
+        views.PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+    ),
+    path(
+        'password-reset-confirm/<uidb64>/<token>/',
+        views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        'password-reset-complete/',
+        views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+    ),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,28 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'profile.html'
+
+
+class PasswordResetView(auth_views.PasswordResetView):
+    subject_template_name = 'mails/password_reset/subject.txt'
+    email_template_name = 'mails/password_reset/body.txt'
+    template_name = 'password_reset.html'
+    success_url = reverse_lazy('accounts:password_reset_done')
+
+
+class PasswordResetDoneView(auth_views.PasswordResetDoneView):
+    template_name = 'password_reset_done.html'
+
+
+class PasswordResetConfirmView(auth_views.PasswordResetConfirmView):
+    success_url = reverse_lazy('accounts:password_reset_complete')
+    template_name = 'password_reset_confirm.html'
+
+
+class PasswordResetCompleteView(auth_views.PasswordResetCompleteView):
+    template_name = 'password_reset_complete.html'
